569279fec4286f0f43359577e9d814d796f7c3ce
varunkumar06 <varun_ec109@yahoo.co.in>
2017-01-22 18:33:23 +0530

    Spring Project

---
+++ MavenWebApp/src/main/resources/com/learn/controllers/HelloController.java
@@ -0,0 +1,13 @@
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	   public String printHello(ModelMap model) {
+	      model.addAttribute("message", "Hello Spring MVC Framework!");
+
+	      return "hello";
+	   }
+
+}
